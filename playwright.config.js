7ced6bf4eeee9ec00d841d4b1a84ae450dff1105
Alexander Lazaris <alex.lazaris617@gmail.com>
2025-10-22 11:36:52 +1100

    fix: many fixes, new target app, page objects refined

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,6 @@
-// @ts-check
 const { defineConfig, devices } = require('@playwright/test');
 require('dotenv').config();
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
 
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -25,7 +16,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
